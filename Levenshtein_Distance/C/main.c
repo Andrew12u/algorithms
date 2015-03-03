8ac11ae743a115c8dd2eb25386e63156093e5f45
rjz <rj@versal.com>
2015-03-02 18:29:55 -0800

    Adds tests
    Extracts library function for testability and sets up a basic CUnit
    harness around it.
    
    CUnit documentation available at http://cunit.sourceforge.net/

--- Levenshtein_Distance/C/main.c
+++ Levenshtein_Distance/C/main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+
+#include "levenshtein.h"
+
 /**
  * @author Andrew Izu
  *
@@ -28,30 +31,11 @@ int main(int argc, char *argv[]){
   strcpy(string_1,argv[1]);
   strcpy(string_2,argv[2]);
   /**/
-  char *larger_string;
-  char *shorter_string;
-  if(strlen(string_2) > strlen(string_1)){
-    larger_string  = string_2;
-    shorter_string = string_1;
-  }else{
-    larger_string  = string_1;
-    shorter_string = string_2;
-  }
-  int min_numCharsDiffer = strlen(larger_string) - strlen(shorter_string);
-  /**  Technically, we don't really need a stack for c.
-    *  We can just iterate through individual strings,
-    *  as they are already arrays.
-    */
-  int add_numCharsDiffer = 0;
-  int i;
-  for(i=0; i<(strlen(larger_string)-min_numCharsDiffer);i++){
-      if(shorter_string[i] != larger_string[i]){
-        add_numCharsDiffer++;
-      }
-  }
-  printf("Levenshtein Distance:%d\n",(min_numCharsDiffer + add_numCharsDiffer));
+
+  printf("Levenshtein Distance:%d\n", distance(string_1, string_2));
   /*Don't forget to release memory*/
   free(string_2);
   free(string_1);
   exit(1);
 }
+
